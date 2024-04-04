054149913f445ded7fa03744c618c09b97e2e4f6
KIMGANGYEON <rkddus3177@naver.com>
2024-04-04 22:43:17 +0900

    아자아자

---
+++ webtube/src/components/section/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface MainProps {}
+
+const Main: React.FunctionComponent<MainProps> = () => {
+  return (
+    <div id="main" role="main">
+      Main
+    </div>
+  );
+};
+
+export default Main;
