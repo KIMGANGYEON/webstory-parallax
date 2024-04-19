d33b7fc685786967d3b1dd433f6f108584f0877c
KIMGANGYEON <rkddus3177@naver.com>
2024-04-19 19:17:57 +0900

    gsap

--- gsapjy/src/Project/Project02.tsx
+++ gsapjy/src/Project/Project02.tsx
@@ -3,9 +3,85 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect, useRef } from "react";
 
 function Project02() {
-  function Project02() {
-    useEffect(() => {}, []);
-  }
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".con02 ul",
+        start: "top 90%",
+        end: "20% 100%",
+        scrub: 4,
+        // markers: true,
+      },
+    });
+    const cards = document.querySelectorAll(".con02 li");
+
+    cards.forEach((card, index) => {
+      tl.to(
+        card,
+        {
+          y: "-400px",
+          duration: 1,
+          ease: "none",
+        },
+        index * 0.2
+      );
+    });
+  }, []);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".con03",
+        start: "top 100%",
+        end: "100% 100%",
+        scrub: 3,
+        markers: true,
+      },
+    });
+    tl.fromTo(
+      ".con03 span.a",
+      {
+        y: "400%",
+      },
+      { y: "0" },
+      1.2
+    );
+    tl.fromTo(
+      ".con03 span.b",
+      {
+        y: "400%",
+      },
+      { y: "0" },
+      1.5
+    );
+    tl.fromTo(
+      ".con03 span.c",
+      {
+        y: "400%",
+      },
+      { y: "0" },
+      1.7
+    );
+    tl.fromTo(
+      ".con03 span.d",
+      {
+        y: "400%",
+      },
+      { y: "0" },
+      1.9
+    );
+    tl.fromTo(
+      ".con03 span.e",
+      {
+        y: "400%",
+      },
+      { y: "0" },
+      2.1
+    );
+  });
+
   return (
     <>
       <div className="Project02">
